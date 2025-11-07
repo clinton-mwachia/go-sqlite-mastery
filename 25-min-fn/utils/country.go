4dba6f62952a68047b712121466d8153e9487330
Clinton Moshe <clintonmwachia9@gmail.com>
2025-11-07 11:02:05 +0300

    ADDED: Min Function

---
+++ 25-min-fn/utils/country.go
@@ -0,0 +1,79 @@
+package utils
+
+import (
+	"database/sql"
+	"encoding/csv"
+	"os"
+	"strconv"
+
+	"github.com/clinton-mwachia/go-sqlite-mastery/25-min-fn/models"
+	_ "github.com/glebarez/go-sqlite"
+)
+
+func CreateTable(db *sql.DB) (sql.Result, error) {
+	sql := `CREATE TABLE IF NOT EXISTS countries (
+        id INTEGER PRIMARY KEY,
+        name     TEXT NOT NULL,
+        population INTEGER NOT NULL,
+        area INTEGER NOT NULL
+    );`
+
+	return db.Exec(sql)
+}
+
+func ReadCSV(filename string) ([]models.Country, error) {
+	// Open the CSV file
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	// Read the CSV file
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse the CSV file
+	var countries []models.Country
+	for _, record := range records[1:] { // Skip header row
+		population, err := strconv.Atoi(record[1])
+		if err != nil {
+			return nil, err
+		}
+		area, err := strconv.ParseFloat(record[2], 64)
+		if err != nil {
+			return nil, err
+		}
+		country := models.Country{
+			Name:       record[0],
+			Population: population,
+			Area:       area,
+		}
+		countries = append(countries, country)
+	}
+
+	return countries, nil
+}
+
+func Insert(db *sql.DB, c *models.Country) (int64, error) {
+	sql := `INSERT INTO countries (name, population, area) 
+            VALUES (?, ?, ?);`
+	result, err := db.Exec(sql, c.Name, c.Population, c.Area)
+	if err != nil {
+		return 0, err
+	}
+	return result.LastInsertId()
+}
+
+func GetMinPopulation(db *sql.DB) (int, error) {
+	query := `SELECT MIN(population) FROM countries`
+	var min int
+	err := db.QueryRow(query).Scan(&min)
+	if err != nil {
+		return 0, err
+	}
+	return min, nil
+}
