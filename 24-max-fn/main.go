fa68e7e0099e3a741b962cf05b0e8d70d166d57f
Clinton Moshe <clintonmwachia9@gmail.com>
2025-11-07 10:32:35 +0300

    ADDED: Max Function

---
+++ 24-max-fn/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/clinton-mwachia/go-sqlite-mastery/24-max-fn/utils"
+	_ "github.com/glebarez/go-sqlite"
+)
+
+func main() {
+	// connect to the SQLite database
+	db, err := sql.Open("sqlite", "./test.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer db.Close()
+
+	// create the countries table
+	_, err = utils.CreateTable(db)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Table countries was created successfully.")
+	// read the CSV file
+	countries, err := utils.ReadCSV("countries.csv")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// insert the data into the SQLite database
+	for _, country := range countries {
+		_, err := utils.Insert(db, &country)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+	}
+
+	pop, err := utils.GetMaxPopulation(db)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Max Pop: %d\n", pop)
+
+}
